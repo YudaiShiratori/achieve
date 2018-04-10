bcd42609d0b45415c61be89c5f6d6ad8a7e741ec
Yudai <toritorikebabu1023x@gmail.com>
2018-04-10 07:37:24 +0000

    fin!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,17 @@ class ContactsController < ApplicationController
   end
   
   def create
-    @contact = Contact.create(contact.id)
+    @contact = Contact.create(contact_params)
     if @contact.save
-      redirect_to new_contact_path
+      redirect_to new_contact_path, notice:"お問い合わせを送信しました！"
     else
       render "new"
     end
   end
   
+  private
+  def contact_params
+    params.require(:contact).permit(:name,:email,:content)
+  end
+
 end
